ae3b5ebf0a87adee5a50aa1901f8557ca149f4ac
LutendoMuthala <lutendo.muthala@capaciti.org.za>
2025-03-14 10:38:15 +0200

    initial version of my portfolio

---
+++ index.js
@@ -0,0 +1,12 @@
+// Get the button by its id
+const downloadButton = document.getElementById('downloadButton');
+
+// Add an event listener to the button
+downloadButton.addEventListener('click', function() {
+
+    alert('You are about to download my resume.');
+    window.location.href = 'LutendoMuthalaCV.pdf'; 
+});
+
+
+
